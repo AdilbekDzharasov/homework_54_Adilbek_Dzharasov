751e6ee5a014f132de2fa005ef14ff89f30d189f
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-09-27 17:18:24 +0600

    Добавил файл urls и прописал пути для всех страниц

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -4,11 +4,13 @@ from webapp.views.base import products_view
 from webapp.views.products_add import product_add_view
 from webapp.views.products_add import product_view
 from webapp.views.categories_add import category_add_view
+from webapp.views.delete_view import delete_product
 
 urlpatterns = [
     path('', products_view, name='home'),
     path('products/add/', product_add_view, name='products_add'),
     path('products/<int:pk>', product_view, name='product_detail'),
+    path('products/delete/<int:pk>', delete_product, name='product_delete'),
     path('categories/add/', category_add_view, name='categories_add'),
 ]
 
